832f59f5d2f7660d560c9800c5b201cf80af5d05
7rahaf <445206408@student.ksu.edu.sa>
2025-07-11 00:27:39 +0300

    Add day4.py - greet and calculate

---
+++ day4.py
@@ -0,0 +1,15 @@
+
+nam = input("Enter your name: ")
+def great(Name): print("Hello,", Name)
+
+
+def calculate(a, b): return (a + b)
+
+
+c = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+great(nam)
+
+result = calculate(c, b)
+print(f"The result is{result}")
